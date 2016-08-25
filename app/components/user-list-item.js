3c979a5a31265fe000dd4704a0ad0467d11332d8
Matthew Meyers <m.j.mey3rs@gmail.com>
2016-08-25 15:29:19 -0500

    Removing transition.js

--- app/components/user-list-item.js
+++ app/components/user-list-item.js
@@ -6,9 +6,6 @@ export default Component.extend({
   classNames: ['item-content'],
   classNameBindings: ['isRating'],
   tagName: 'li',
-  didRender() {
-    this.$().attr('data-user-id', this.get('user.id'));
-  },
   click() {
     this.get('toggleIsRating')(this.get('user'));
   }
